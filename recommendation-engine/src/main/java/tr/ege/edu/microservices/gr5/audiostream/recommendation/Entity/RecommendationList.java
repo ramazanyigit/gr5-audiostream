9660cc1a43c1975a109b4042c03e908173eaf0c4
M. Yusuf Ozkan <myozkan@yandex.com>
2021-06-24 01:26:29 +0300

    added RecommendationList entity.

--- recommendation-engine/src/main/java/tr/ege/edu/microservices/gr5/audiostream/recommendation/Entity/RecommendationList.java
+++ recommendation-engine/src/main/java/tr/ege/edu/microservices/gr5/audiostream/recommendation/Entity/RecommendationList.java
@@ -1,5 +1,29 @@
 package tr.ege.edu.microservices.gr5.audiostream.recommendation.Entity;
 
-public class RecommendationList {
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.List;
+import java.util.UUID;
 
+public class RecommendationList {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private UUID recommendationId;
+    private List<UUID> recommendedSongs;
+
+    public RecommendationList() {
+    }
+
+    public RecommendationList(List<UUID> recommendedSongs) {
+        this.recommendedSongs = recommendedSongs;
+    }
+
+    public List<UUID> getRecommendedSongs() {
+        return recommendedSongs;
+    }
+
+    public void setRecommendedSongs(List<UUID> recommendedSongs) {
+        this.recommendedSongs = recommendedSongs;
+    }
 }
