2678b0f53c2c23dbf3f156e0013f6ae81900abee
sudeadiguzel <sudeadiguzeell@gmail.com>
2021-06-24 09:52:09 +0300

    update service

--- playlist-service/src/main/java/tr/ege/edu/microservices/gr5/audiostream/playlist/repository/PlaylistRepository.java
+++ playlist-service/src/main/java/tr/ege/edu/microservices/gr5/audiostream/playlist/repository/PlaylistRepository.java
@@ -4,9 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import tr.ege.edu.microservices.gr5.audiostream.playlist.model.Playlist;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface PlaylistRepository extends JpaRepository<Playlist, UUID> {
-    
+    Optional<Playlist> getByPlaylistId(UUID id);
+
+
+
 }
