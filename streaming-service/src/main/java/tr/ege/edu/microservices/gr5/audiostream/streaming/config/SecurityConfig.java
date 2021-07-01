ba44b4582fe2a8190ef781d374078d41a5d7ec30
ramazanyigit <54190298+ramazanyigit@users.noreply.github.com>
2021-07-01 17:38:39 +0300

    Added Keycloak JWK authentication on microservices.

--- streaming-service/src/main/java/tr/ege/edu/microservices/gr5/audiostream/streaming/config/SecurityConfig.java
+++ streaming-service/src/main/java/tr/ege/edu/microservices/gr5/audiostream/streaming/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package tr.ege.edu.microservices.gr5.audiostream.streaming;
+package tr.ege.edu.microservices.gr5.audiostream.streaming.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -15,14 +15,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors()
                 .and()
-//                .authorizeRequests()
-//                .antMatchers(HttpMethod.GET, "/user/info", "/api/foos/**")
-//                .hasAuthority("SCOPE_read")
-//                .antMatchers(HttpMethod.POST, "/api/foos")
-//                .hasAuthority("SCOPE_write")
-//                .anyRequest()
-//                .authenticated()
-//                .and()
                 .oauth2ResourceServer()
                 .jwt();
     }
