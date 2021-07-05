c1ca627c015bce77001907afd07703be9589c993
sudeadiguzel <i.sude.adiguzel@gmail.com>
2021-07-05 23:31:59 +0300

    update playlist models.

--- playlist-service/src/main/java/tr/ege/edu/microservices/gr5/audiostream/playlist/model/Playlist.java
+++ playlist-service/src/main/java/tr/ege/edu/microservices/gr5/audiostream/playlist/model/Playlist.java
@@ -1,13 +1,16 @@
 package tr.ege.edu.microservices.gr5.audiostream.playlist.model;
+
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
+
 import javax.persistence.*;
 import java.util.Set;
 import java.util.UUID;
 
-@Table(name="ps_playlist")
 @Entity
 @Data
+@NoArgsConstructor
 public class Playlist {
     @Id
     @GeneratedValue(generator = "UUID")
@@ -15,14 +18,17 @@ public class Playlist {
             name="UUID",
             strategy = "org.hibernate.id.UUIDGenerator"
     )
-    @Column(name = "id", updatable = false, nullable = false)
+    @Column(updatable = false, nullable = false)
     private UUID id;
 
-    @Column(name="name")
+    @Column(nullable = false, updatable = false)
+    private UUID userId;
+
+    @Column(nullable = false)
     private String name;
 
     @OneToMany
     @JoinColumn
-    private Set<Song> songSet;
+    private Set<Song> songs;
 
 }
--- playlist-service/src/main/java/tr/ege/edu/microservices/gr5/audiostream/playlist/model/Song.java
+++ playlist-service/src/main/java/tr/ege/edu/microservices/gr5/audiostream/playlist/model/Song.java
@@ -2,16 +2,25 @@ package tr.ege.edu.microservices.gr5.audiostream.playlist.model;
 
 import lombok.Data;
 
-import javax.persistence.*;
-import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
-@Table(name="ps_song")
 @Entity
 @Data
 public class Song {
     @Id
-    @Column(name = "id", updatable = false, nullable = false)
+    @Column(updatable = false, nullable = false)
     private UUID id;
 
+    @Column(columnDefinition = "TIMESTAMP WITH TIME ZONE", nullable = false, updatable = false)
+    private OffsetDateTime date;
+
+    @PrePersist
+    public void prePersist() {
+        date = OffsetDateTime.now();
+    }
 }
