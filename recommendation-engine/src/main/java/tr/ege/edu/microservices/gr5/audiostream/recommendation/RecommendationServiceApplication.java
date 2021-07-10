8f25f8c8008309bd2e6609819fe457d0c40b5c73
ramazanyigit <54190298+ramazanyigit@users.noreply.github.com>
2021-07-10 15:32:37 +0300

    fix feign client problems.

--- recommendation-engine/src/main/java/tr/ege/edu/microservices/gr5/audiostream/recommendation/RecommendationServiceApplication.java
+++ recommendation-engine/src/main/java/tr/ege/edu/microservices/gr5/audiostream/recommendation/RecommendationServiceApplication.java
@@ -2,7 +2,9 @@ package tr.ege.edu.microservices.gr5.audiostream.recommendation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class RecommendationServiceApplication {
 
